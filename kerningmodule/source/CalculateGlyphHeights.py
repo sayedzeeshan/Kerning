4bf309c93fd525aff0cf76256a0f504bcedc00f7
sayedzeeshan <sayedzeeshan@gmail.com>
2021-08-22 22:06:11 +0800

    Addition of haroof processing code, thorough testing required

--- kerningmodule/source/CalculateGlyphHeights.py
+++ kerningmodule/source/CalculateGlyphHeights.py
@@ -12,14 +12,27 @@ nBins = 11
 minHeight = 710
 maxHeight = 780
 shiftX = 200
-scaling = 5
+scaling = 4.9 #2048em font assumption 
 DESC_RATE = 0.2
 ASC_RATE_1 = 4.0
 ASC_RATE_2 = 8.0
+HAROOF_BOTTOM_DEFAULT = 300
+HAROOF_TOP_DEFAULT = 370
+HAROOF_TOP_EXCEPTION = 440
+SHORT_GLYPH_BOUNDARY = 61
+ASC_RATE_EXCEPTION = 4
+ASC_RATE_HAROOF = 7
+ASC_RATE_DEFAULT_HAROOF = 4
+SYMBOL_TOP_DEFAULT = 460
+SYMBOL_BOTTOM_DEFAULT = 100
+
 nPixelScan = 9
 dXValid = [10,20,40,50,100]
 LookUp = {} #instantiate an empty dictionary
-def CalculateGlyphHeights(baseDir,dX,debugArgs):
+haroofExceptions = ["alef.png","alefwah"]
+
+
+def CalculateGlyphHeights(baseDir,dX,enableKasheeda):
     if dX not in dXValid:
         print("dX value is not correct. It should be one of the folowing values: ")
         print(dXValid)
@@ -31,7 +44,27 @@ def CalculateGlyphHeights(baseDir,dX,debugArgs):
     symbolDir = baseDir+"/Symbols/"
     kasheedaDir = baseDir+"/Ligatures_Kashida/"
     kasheedaHaroofDir = baseDir+"/Haroof_Kashida/"
-    for filepath in glob.iglob(ligatureDir + '**/*.png', recursive=True):
+    numGlyphs += ProcessRegularGlyphs(ligatureDir,dX)
+    print("Number of  glyphs processed: " + str(numGlyphs))
+    # regular Haroof processing
+    numGlyphs += ProcessHaroofGlyphs(haroofDir,dX)
+    print("Number of glyphs processed: " + str(numGlyphs))
+    if enableKasheeda:
+        numGlyphs += ProcessRegularGlyphs(kasheedaDir,dX)
+        print("Number of  glyphs processed: " + str(numGlyphs))
+        # regular Haroof processing
+        numGlyphs += ProcessHaroofGlyphs(kasheedaHaroofDir,dX)
+        print("Number of glyphs processed: " + str(numGlyphs))
+
+    print("All glyphs processed successfully")
+    return LookUp
+
+def removeNumbers(name):
+    print("to be implemented")
+
+def ProcessRegularGlyphs(dir,dX):
+     numGlyphs = 0
+     for filepath in glob.iglob(dir + '**/*.png', recursive=True):
         filename = os.path.basename(filepath)
         print(filename)
         im = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
@@ -77,7 +110,7 @@ def CalculateGlyphHeights(baseDir,dX,debugArgs):
             else:
                 hmax = start_h[k]
 
-        if nW < 61:
+        if nW < SHORT_GLYPH_BOUNDARY:
             rate = ASC_RATE_2
         else:
             rate = ASC_RATE_1
@@ -106,8 +139,148 @@ def CalculateGlyphHeights(baseDir,dX,debugArgs):
         extents[0:nW,1] = end_h[0::dX]
 
         LookUp[filename[0:-4]] = extents
-    print("All glyphs processed successfully")
-    return LookUp
+     return numGlyphs
 
-def removeNumbers(name):
-    print("to be implemented")
+def ProcessHaroofGlyphs(dir,dX):
+    numGlyphs = 0
+    for filepath in glob.iglob(dir + '**/*.png', recursive=True):
+        exceptionFlag = 0
+        filename = os.path.basename(filepath)
+        print(filename)
+        im = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
+        H, W = im.shape
+        if H < minHeight or H > maxHeight:
+            print("Image height should be between "+str(minHeight)+" pixels and "+str(maxHeight)+" pixels.")
+            print("Program exiting now")
+            return 0
+        numGlyphs += 1
+        nbinsTemp = nBins*dX
+        shiftXTemp = shiftX/dX
+        divisor = int(np.floor(shiftXTemp/scaling))
+        nW = int(np.floor((W-nPixelScan)/divisor) + 1)
+        
+        if nW > nbinsTemp:
+            nW = nbinsTemp
+       
+        starting = np.ones((H,nW),dtype=np.int8) #bottom
+        ending = np.ones((H,nW),dtype=np.int8) #top 
+        start_h = HAROOF_BOTTOM_DEFAULT*np.ones((nbinsTemp,),dtype=int)
+        extents = np.zeros((nBins,2),dtype=int)
+        if(filename[0:-4] in haroofExceptions):
+            end_h = HAROOF_TOP_EXCEPTION*np.ones((nbinsTemp,),dtype=int)
+        else:
+            end_h = HAROOF_TOP_DEFAULT*np.ones((nbinsTemp,),dtype=int)
+
+        for j in range(0,nW):
+            for i in range(0,nPixelScan):
+                starting[0:H,j] = starting[0:H,j] & im[0:H,W - j*divisor - i - 1]
+                ending[0:H,j] = ending[0:H,j] & im[0:H,i + j*divisor]
+
+        for j in range(0,nW):
+            a  = np.argwhere(starting[0:H,j] == 0)
+            if not a.any():
+                start_h[j] = -1
+                end_h[j] = -1
+            else:
+                start_h[j] = a[-1]
+                end_h[j] = a[1]
+                start_h[j] = H - start_h[j]
+                end_h[j] = H - end_h[j]
+        
+        hmax = start_h[0]
+        for k in range(1,nbinsTemp):
+            if start_h[k] > hmax:
+                start_h[k] = hmax
+            else:
+                hmax = start_h[k]
+
+        if nW < SHORT_GLYPH_BOUNDARY:
+            if exceptionFlag:
+                rate = ASC_RATE_EXCEPTION
+            else:
+                rate = ASC_RATE_HAROOF
+        else:
+            rate = ASC_RATE_DEFAULT_HAROOF
+
+        hmin = end_h[0]
+        for k in range(1,nbinsTemp):
+            if end_h[k] < hmin:
+                end_h[k] = hmin
+            else:
+                hmin = end_h[k]
+
+        hmin = end_h[nW-1]
+        for k in range(nW,nbinsTemp):
+            if(hmin - (k-nW)*rate > H):
+                end_h[k] = 0
+            else:
+                end_h[k] = int(hmin + (k-nW)*rate)
+        extents[0:nW,0] = start_h[0::dX]
+        extents[0:nW,1] = end_h[0::dX]
+
+        LookUp[filename[0:-4]] = extents
+    return numGlyphs
+
+def ProcessSymbolGlyphs(dir,dX):
+    numGlyphs = 0
+    for filepath in glob.iglob(dir + '**/*.png', recursive=True):
+        exceptionFlag = 0
+        filename = os.path.basename(filepath)
+        print(filename)
+        im = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
+        H, W = im.shape
+        if H < minHeight or H > maxHeight:
+            print("Image height should be between "+str(minHeight)+" pixels and "+str(maxHeight)+" pixels.")
+            print("Program exiting now")
+            return 0
+        numGlyphs += 1
+        nbinsTemp = nBins*dX
+        shiftXTemp = shiftX/dX
+        divisor = int(np.floor(shiftXTemp/scaling))
+        nW = int(np.floor((W-nPixelScan)/divisor) + 1)
+        
+        if nW > nbinsTemp:
+            nW = nbinsTemp
+       
+        starting = np.ones((H,nW),dtype=np.int8) #bottom
+        ending = np.ones((H,nW),dtype=np.int8) #top 
+        start_h = SYMBOL_BOTTOM_DEFAULT*np.ones((nbinsTemp,),dtype=int)
+        extents = np.zeros((nBins,2),dtype=int)
+        end_h = SYMBOL_TOP_DEFAULT*np.ones((nbinsTemp,),dtype=int)
+
+        for j in range(0,nW):
+            for i in range(0,nPixelScan):
+                starting[0:H,j] = starting[0:H,j] & im[0:H,W - j*divisor - i - 1]
+                ending[0:H,j] = ending[0:H,j] & im[0:H,i + j*divisor]
+
+        for j in range(0,nW):
+            a  = np.argwhere(starting[0:H,j] == 0)
+            if not a.any():
+                start_h[j] = -1
+                end_h[j] = -1
+            else:
+                start_h[j] = a[-1]
+                end_h[j] = a[1]
+                start_h[j] = H - start_h[j]
+                end_h[j] = H - end_h[j]
+        
+        hmax = start_h[0]
+        for k in range(1,nbinsTemp):
+            if start_h[k] > hmax:
+                start_h[k] = hmax
+            else:
+                hmax = start_h[k]
+
+        hmin = end_h[0]
+        for k in range(1,nbinsTemp):
+            if end_h[k] < hmin:
+                end_h[k] = hmin
+            else:
+                hmin = end_h[k]
+
+        extents[0:nW,0] = start_h[0::dX]
+        extents[0:nW,1] = end_h[0::dX]
+
+        LookUp[filename[0:-4]] = extents
+    return numGlyphs
+    
--- test/TestGlyphHeights.py
+++ test/TestGlyphHeights.py
@@ -1,5 +1,9 @@
 import kerningmodule.source.CalculateGlyphHeights as CGH
 import SaveLoadObjects as SLO
 
-LookUp = CGH.CalculateGlyphHeights("C:/Ligatures",10,0)
-SLO.save_obj(LookUp,'GlyphHeightsDictionary')
+def HaroofHeightTest():
+    numGlyphs = CGH.ProcessHaroofGlyphs("C:/Ligatures/Haroof_Regular/",10)
+    assert numGlyphs == 46
+def AllGlyphsHeighTest():
+    LookUp = CGH.CalculateGlyphHeights("C:/Ligatures",10,0)
+    SLO.save_obj(LookUp,'GlyphHeightsDictionary')
