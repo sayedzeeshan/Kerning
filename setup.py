0e6773a2c0ea1612aaecb623a802acd5868ff1fb
sayedzeeshan <sayedzeeshan@gmail.com>
2021-08-30 20:21:42 +0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'ligature_kerning',
+  packages = find_packages(),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Nasteeq ligatures kerning for OpenType (TruType) Fonts',
+  author = 'Sayed Zeeshan Asghar',
+  author_email = 'sayedzeeshan@gmail.com',
+  url = 'https://github.com/sayedzeeshan/Kerning',
+  keywords = [
+    'nastaliq',
+    'kerning',
+    'auto-kerning',
+    'opentype',
+    'fonts'
+  ],
+  install_requires=[
+    'numpy >= 1.0',
+    'matplotlib >= 1.0',
+    'opencv-python >= 4.5.2.0'
+  ],
+  classifiers=[
+    'Development Status :: 1 - Beta',
+    'Intended Audience :: Users',
+    'Topic :: Fonts :: Kerning',
+    'License :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
