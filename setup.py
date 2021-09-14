8f0ee573c58948adae10e653b25bf4f871e5483c
sayedzeeshan <sayedzeeshan@gmail.com>
2021-09-14 21:58:30 +0800

    v0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'ligature_kerning',
   packages = find_packages(),
-  version = '0.0.4',
+  version = '0.0.5',
   license='MIT',
   description = 'Nasteeq ligatures kerning for OpenType (TruType) Fonts',
   author = 'Sayed Zeeshan Asghar',
